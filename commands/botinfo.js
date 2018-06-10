f9826bf9d31cadd3bb3eae409aaa489f0d6c83f2
Densuki <DarkShadow3315@gmail.com>
2018-06-10 18:11:48 -0300

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
       .addField("**Nome do BOT**", bot.user.username)
       .addField("**Criado em**", bot.user.createdAt)
       .addField("**INVITE**, https://discordapp.com/oauth2/authorize?client_id=455472236694863882&permissions=8&scope=bot")
-      .addField("**CRIADOR**, Criado por Por @Yukki Mizuminno 〘Densuki〙#6235 | <@336311215099740160>");
+      .addField("**CRIADOR**, Criado por Por @Yukki Mizuminno 〘Densuki〙#6235");
 
       return message.channel.send(botembed).then(msg => {msg.delete(9000)});
 }
