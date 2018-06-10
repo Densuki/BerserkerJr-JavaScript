7ef265c2c81406fcc013a7afbad9490d1edfb5ff
Densuki <DarkShadow3315@gmail.com>
2018-06-10 18:10:21 -0300

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -8,8 +8,9 @@ module.exports.run = async (bot, message, args) => {
       .setColor("#5F04B4") //ROXO
       .setThumbnail(bicon)
       .addField("**Nome do BOT**", bot.user.username)
-      .addField("**Criado em**", bot.user.createdAt Por <@336311215099740160>)
-       .addField("**INVITE**, https://discordapp.com/oauth2/authorize?client_id=455472236694863882&permissions=8&scope=bot");
+      .addField("**Criado em**", bot.user.createdAt)
+      .addField("**INVITE**, https://discordapp.com/oauth2/authorize?client_id=455472236694863882&permissions=8&scope=bot")
+      .addField("**CRIADOR**, Criado por Por @Yukki Mizuminno 〘Densuki〙#6235 | <@336311215099740160>");
 
       return message.channel.send(botembed).then(msg => {msg.delete(9000)});
 }
